4c7fe5795194537310d6e3f240df40eb60e846cc
Koding <ydatech@ydatech.koding.io>
2016-02-20 19:34:45 -0600

    First Commit

---
+++ efa/WatsonDialog.php
@@ -0,0 +1,22 @@
+<?php
+
+ namespace app\efa;
+ 
+ use yii\httpclient\Client;
+ 
+ class WatsonDialog {
+     
+     
+     const API_URL = 'https://gateway.watsonplatform.net/dialog/api/v1/';
+     const API_URL_EPLORER = 'https://watson-api-explorer.mybluemix.net/dialog/api/v1/';
+     
+     private $username = 'dfd28718-fa2a-458a-9077-a2e3cdc106bc';
+     private $password = 'Nr2RcpM0cukd';
+     
+     
+     
+     
+     
+     
+     
+ }
